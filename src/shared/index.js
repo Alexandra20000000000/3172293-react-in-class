8f160f42fa219bf6c2a221fe2bfa432a5a7adef2
Alexandra 20000000000 <loaizalaura411@gmail.com>
2026-05-25 07:48:19 -0500

    Primer commit

---
+++ src/shared/index.js
@@ -0,0 +1,8 @@
+//Layouts
+
+export { default as AuthLayout } from "./layouts/AuthLayout";
+export { default as DashboardLayout } from "./layouts/DashboardLayout";
+
+
+//Componentes
+export { default as input} from "./components/Input"
